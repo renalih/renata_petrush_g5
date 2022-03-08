b62edf051201e7bdb6bada29d5b05a7087b04651
Renata Petrush <renapet3@gmail.com>
2022-03-08 17:14:03 +0100

    my hometask from week 3 (arraytask)

---
+++ src/main/java/tasks/homework/week3/ArrayTaskRunner.java
@@ -0,0 +1,15 @@
+package tasks.homework.week3;
+
+public class ArrayTaskRunner {
+
+    static ArrayTask arrayTask = new ArrayTask();
+
+    public static void main(String[] args) {
+
+        arrayTask.setArray();
+        arrayTask.sumElementsFromArray(5);
+        arrayTask.returnArray(15);
+        arrayTask.returnArrayByMonth();
+        arrayTask.printPicture();
+    }
+}
