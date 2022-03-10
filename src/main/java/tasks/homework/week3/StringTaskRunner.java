7b9bb9225782dc1a3ad341ff6f3e3f3e7ddec681
Renata Petrush <renapet3@gmail.com>
2022-03-10 22:57:57 +0100

    Done 2 tasks from StringTask

---
+++ src/main/java/tasks/homework/week3/StringTaskRunner.java
@@ -0,0 +1,13 @@
+package tasks.homework.week3;
+
+public class StringTaskRunner {
+
+    static StringTask stringTask = new StringTask();
+
+    public static void main(String[] args) {
+
+        stringTask.duplicateWord("Lepszy wróbel w garści niż gołąb na dachu wróbel gołąb");
+        stringTask.extractNumbers("13jkgk gllg8fn899ff dfkkfkgkYY12 1 45 ЛЩЛ4");
+
+    }
+}
