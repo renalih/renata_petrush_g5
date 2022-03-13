652cbe8951196c68043f9470a7628841761a028f
Renata Petrush <renapet3@gmail.com>
2022-03-13 11:33:37 +0100

    firsst I/O task

--- src/main/java/tasks/homework/week4/NextMonthReader.java
+++ src/main/java/tasks/homework/week4/NextMonthReader.java
@@ -1,24 +1,25 @@
 package tasks.homework.week4;
 
-import java.time.Month;
 import java.util.Scanner;
 
 public class NextMonthReader {
 
     public static void main(String[] args) {
 
+        System.out.println("Enter number of current month: ");
+
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Введите название месяца: ");
-
-        while (scanner.hasNext()) {
-            String incoming = scanner.nextLine();
-            Month month = Month.of();
-            System.out.println(month);
-        }
-
-
+        int incoming = scanner.nextInt();
+        System.out.println(monthDefinition(incoming));
     }
 
+    public static String monthDefinition(int incoming) {
+        String[] month = {"","January", "February","March","April","May","June","July","August",
+                "September","October","November","December"};
+        if (incoming > 0 && incoming < month.length) {
+            return month[incoming + 1];
+        }
+        return "Such month does not exist";
+    }
 
 }
