b0dd2990763626112c7b64307b71a281b04b9840
Renata Petrush <renapet3@gmail.com>
2022-03-17 17:00:07 +0100

    add classwork and small fixes previous tasks

---
+++ src/main/java/tasks/homework/week5/Cars.java
@@ -0,0 +1,29 @@
+package tasks.homework.week5;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Cars {
+
+    public static void main(String[] args) throws IOException {
+
+        Set<String> cars = new HashSet<String>();
+        cars.add("Мерс");
+        cars.add("Ауди");
+        cars.add("Жигуль");
+        cars.add("Рено");
+        cars.add("Жигуль");
+        cars.add("Жигуль");
+        cars.add("Ауди");
+
+        BufferedWriter out = new BufferedWriter(new FileWriter("cars.txt"));
+        for (String s : cars) {
+            out.write("'" + s + "'" + "\n");
+        }
+        out.close();
+
+    }
+}
