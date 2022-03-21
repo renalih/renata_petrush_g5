d53092d36bd285815cc0f60ac8dfaeabf1532929
Renata Petrush <renapet3@gmail.com>
2022-03-21 21:06:22 +0100

    homework of day 1 of week 6 (task 1)

---
+++ src/main/java/tasks/homework/week6/Inches.java
@@ -0,0 +1,17 @@
+package tasks.homework.week6;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Inches {
+    public static void main(String[] args) {
+        List<Integer> listlengths = new ArrayList<>(Arrays.asList
+                (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20));
+
+        double l = listlengths.stream().filter(x -> x % 2 == 0).peek(System.out::println).
+                mapToDouble(x -> (double) (x * 2.54)).sum();
+        System.out.println(l);
+
+    }
+}
